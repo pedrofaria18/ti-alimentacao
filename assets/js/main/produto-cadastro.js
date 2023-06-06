665050ea6b00eb47c2b90533fd3c8a12b2a28bbb
Gabriel Favero <gabriel.o.favero@live.com>
2023-06-06 20:00:21 -0300

    added event listener

--- assets/js/main/produto-cadastro.js
+++ assets/js/main/produto-cadastro.js
@@ -1,3 +1,6 @@
 function _startProdutoCadastro() {
-    
+    const submitButton = document.getElementById("submit");
+    submitButton.addEventListener("click", function() {
+        _submitPedido();
+    });
 }
