5640722b6276f4b17cf8b7fd8af12b28b5ee68a1
Gabriel Favero <gabriel.o.favero@live.com>
2023-06-12 23:07:24 -0300

    Criada página Index

--- fe/assets/js/main/index.js
+++ fe/assets/js/main/index.js
@@ -1,3 +1,19 @@
 function _startIndex(){
-    
+    _get('/products').then(produtos => {
+        const now = new Date();
+        const produtosDiv = document.getElementById("produtos");
+        produtosDiv.innerHTML = produtos.length;
+
+        const produtosDate = document.getElementById("produtosDate");
+        produtosDate.innerHTML = "Atualizado em: " + _dateToFullDateString(now);
+    });
+
+    _get('/orders').then(pedidos => {
+        const now = new Date();
+        const pedidosDiv = document.getElementById("pedidos");
+        pedidosDiv.innerHTML = pedidos.length;
+
+        const pedidosDate = document.getElementById("pedidosDate");
+        pedidosDate.innerHTML = "Atualizado em: " + _dateToFullDateString(now);
+    });
 }
--- fe/assets/js/support/date.js
+++ fe/assets/js/support/date.js
@@ -197,3 +197,29 @@ function _getDayOfTheWeek(key) {
     return daysOfWeek[dayOfWeekIndex];
 }
 
+function _dateToFullDateString(date){
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+
+    if (day < 10){
+        day = "0" + day;
+    }
+
+    if (month < 10){
+        month = "0" + month;
+    }
+
+    if (hour < 10){
+        hour = "0" + hour;
+    }
+
+    if (minute < 10){
+        minute = "0" + minute;
+    }
+
+    return day + "/" + month + "/" + year + " " + hour + ":" + minute;
+}
